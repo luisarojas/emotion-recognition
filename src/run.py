8a3cdfe5dab1e162de223b77f834389647ecc59d
Luisa Rojas <r.luisaf@gmail.com>
2018-11-11 15:39:11 -0500

    Refactored project

--- src/run.py
+++ src/run.py
@@ -1,21 +1,14 @@
-# from keras.preprocessing.image import load_img
 import numpy as np
-from keras.models import load_model
-from utils.datasets import get_labels
-from utils.inference import load_detection_model
-from utils.inference import load_image
-from utils.inference import detect_faces
-from utils.inference import draw_bounding_box
-from utils.inference import draw_text
-from utils.inference import apply_offsets
-from utils.preprocessor import preprocess_input
-
 import cv2
+
+from keras.models import load_model
+from utils.helper import apply_offsets,preprocess_input
 
 WEIGHTS_EMOTION="./res/saved_weights/emotion_model.hdf5"
 HAAR_CASCADE="./res/opencv/casc/haarcascade_frontalface_default.xml"
 IMAGE_IN="./res/img/abba.png"
 IMAGE_OUT="./out/predicted.png"
+EMOTION_LABELS = {0:'angry', 1:'disgust', 2:'fear', 3:'happy', 4:'sad', 5:'surprise', 6:'neutral'}
 
 if __name__ == "__main__":
 
@@ -24,12 +17,11 @@ if __name__ == "__main__":
     # OPEN CV2 INIT
     font = cv2.FONT_HERSHEY_SIMPLEX # font to use in output text
     emotion_offsets = (0,0) # set for drawing bounding boxes
-    face_detection = load_detection_model(HAAR_CASCADE)
+    face_detection = cv2.CascadeClassifier(HAAR_CASCADE)
 
-    #Load the trained model + labels
-    emotion_classifier = load_model(WEIGHTS_EMOTION, compile=False) # load weights
+    # load the trained model from keras, and use the given weights
+    emotion_classifier = load_model(WEIGHTS_EMOTION, compile=False)
     emotion_target_size = emotion_classifier.input_shape[1:3]
-    emotion_labels = get_labels('fer2013') #{0: 'angry', 1: 'disgust', 2: 'fear', 3: 'happy', 4: 'sad', 5: 'surprise', 6: 'neutral'}
 
     video_capture = cv2.VideoCapture(0)
 
@@ -49,7 +41,15 @@ if __name__ == "__main__":
         gray_image = gray_image.astype('uint8')
 
         #Get the faces from the image
-        faces = detect_faces(face_detection, gray_image)
+        faces = face_detection.detectMultiScale(
+            gray, # greyscale image
+            scaleFactor=1.3, # compensate for faces being closer
+                             # or further away from the camera
+            minNeighbors=5, # how many objects are detected near the
+                            # current one before it declares the face found
+            minSize=(20, 20) # gives the size of each window
+            # flags=cv2.cv.CV_HAAR_SCALE_IMAGE # not needed in cv v3
+        )
 
         for face_coords in faces:
             # Obtain the coordinates from each face to extract them from original image
@@ -67,17 +67,24 @@ if __name__ == "__main__":
             gray_face = np.expand_dims(gray_face, -1)
 
             # classify current face
+            # get percentages for each label
             emotion_label_arg = np.argmax(emotion_classifier.predict(gray_face))
 
             # get label text corresponding to the number returned by the model
-            emotion_text = emotion_labels[emotion_label_arg]
+            emotion_text = EMOTION_LABELS[emotion_label_arg]
 
-            color = (255,0,0)
-            
             # draw box around face
-            draw_bounding_box(face_coords, frame, color)
+            box_color = (255, 0, 0)
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y), (x+w, y+h), box_color, 2)
+
             # draw text around face
-            draw_text(face_coords, frame, emotion_text, 0, -20, 1, 2)
+            text_color = (0, 255, 0)
+            x, y = face_coords[:2]
+            font_scale = 2
+            thickness = 2
+            # draw_text(face_coords, frame, emotion_text, 0, -20, 1, 2)
+            cv2.putText(frame, emotion_text, (x + emotion_offsets[0], y + emotion_offsets[1]), font, font_scale, text_color, thickness, cv2.LINE_AA)
 
         cv2.imshow('Video', frame)
 
---
+++ src/utils/helper.py
@@ -0,0 +1,14 @@
+# Taken from https://github.com/oarriaga/face_classification
+
+def apply_offsets(face_coordinates, offsets):
+    x, y, width, height = face_coordinates
+    x_off, y_off = offsets
+    return (x - x_off, x + width + x_off, y - y_off, y + height + y_off)
+
+def preprocess_input(x, v2=True):
+    x = x.astype('float32')
+    x = x / 255.0
+    if v2:
+        x = x - 0.5
+        x = x * 2.0
+    return x
